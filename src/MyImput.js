905cc8da322d6fc5a189005f7d45fb592f7f0ab7
edixonMoreno <edixon.moreno.fuentes@gmail.com>
2022-08-28 22:45:30 -0400

    Se crea codigo del login de la aplicacion con validaciones incluidas

--- src/MyImput.js
+++ src/MyImput.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import "./MyInput.css"
+import {Formik} from "formik";
 
 
 const MyImput = () => {
 
+    const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
     // const [inputNombre, cambiarInputNombre] = useState("");
     // const [inputCorreo, cambiarInputCorreo] = useState("");
 
@@ -24,17 +27,80 @@ const MyImput = () => {
 
     return (
     <> 
-        <form className="formulario">
+    <Formik 
+    initialValues= {{
+        nombre: "",
+        correo: ""
+    }}
+
+    validate={(valores) => {
+        let errores = {};
+        if(!valores.nombre){
+            errores.nombre = "Por favor ingresa un nombre"
+        } else if( !/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)) {
+           errores.nombre = "El nombre solo puede contener letras y espacios"; 
+        } 
+        
+        if(!valores.correo){
+            errores.correo = "Por favor ingresa un correo"
+        }else if( !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)) {
+            errores.correo = "Debe ingresar un correo valido";
+         }
+        return errores;
+ } }
+    onSubmit={(valores, {resetForm}) =>{
+        resetForm();
+        cambiarFormularioEnviado(true);
+        setTimeout(() => {
+            cambiarFormularioEnviado(false)
+        }, 5000);
+        console.log("Formulario enviado!");
+    }}
+    >
+       
+    {({values, touched, errors, handleSubmit, handleChange, handleBlur}) => (
+
+        <form className="formulario" 
+        onSubmit= {handleSubmit} 
+        action="direccion-de-envio-de-datos" 
+        method="get">
+            
+            
             <div>
-            <label htmlfor="nombre">Nombre</label>
-            <input type="text" id="nombre" name="nombre" placeolder="Jon Dee"></input>
+            <label htmlFor="nombre">Nombre</label>
+            <input 
+            type="text" 
+            id="nombre" 
+            name="nombre" 
+            placeholder="Jon Dee" 
+            value={values.nombre}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            >
+                
+            </input>
+            {touched.nombre && errors.nombre && <div className="error">{errors.nombre}</div>}
             </div>
             <div>
-            <label htmlfor="correo">Correo</label>
-            <input type="email" id="correo" name="correo" placeolder="correo@correo.com"></input>
+            <label htmlFor="correo">Correo</label>
+            <input 
+            type="email" 
+            id="correo" 
+            name="correo" 
+            placeholder="correo@correo.com" 
+            value={values.correo}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            ></input>
+            {touched.correo && errors.correo && <div className="error">{errors.correo}</div>}
             </div>
         <button type="submit">Enviar</button>
-        </form>
+       {formularioEnviado && <p className="exito">Formulario enviado con exito!</p>}
+                </form>
+
+    )}
+
+    </Formik>
     </>
     )
 }
